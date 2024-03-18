34223de863bf4c0dfcebecb11cf7d821e405571c
Yacine Bendjebbar <106481756+yxcinebendjebbar@users.noreply.github.com>
2024-03-18 23:21:42 +0100

    initialized frontend (client)
    initialized frontend (client): React (vite) + tailwindcss + react router

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className='w-full h-screen flex justify-center items-center'>
+      <h1 className='text-xl'>React App</h1>
+    </div>
+  );
+}
+
+export default App;
