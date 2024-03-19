1a20089b09bc1478522a6b43ddce10518dcf0929
Yacine Bendjebbar <106481756+yxcinebendjebbar@users.noreply.github.com>
2024-03-19 12:16:48 +0100

    feat: added login route
    feat: added login route

--- server/routes/StudentRoutes.js
+++ server/routes/StudentRoutes.js
@@ -8,9 +8,25 @@ const router = express.Router();
 router.post("/signup", async (req, res) => {
   try {
     const newStudent = new Student(req.body);
-    console.log(newStudent);
     const student = await newStudent.save();
     res.status(201).json(student);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Student login
+
+router.post("/login", async (req, res) => {
+  try {
+    const student = await Student.findOne({
+      email: req.body.email,
+      passwrd: req.body.passwrd,
+    });
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+    res.status(200).json(student);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
