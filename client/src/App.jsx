95fccc3e217eef97f71765838c00d9afe3c46217
Yacine Bendjebbar <106481756+yxcinebendjebbar@users.noreply.github.com>
2024-04-03 00:21:30 +0100

    feat: added authentication
    feat: added authentication (login signup home pages)
    feat: added navbar mobile view and desktop view for other pages different from landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,46 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useAuth, AuthProvider } from "./hooks/useAuth";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import HomePage from "./pages/HomePage";
+
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignupPage />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signup' element={<SignupPage />} />
+
+          <Route
+            path='/home'
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAuth();
+  if (!user) {
+    // user is not authenticated
+    return <Navigate to='/login' />;
+  }
+  return children;
+};
 
 export default App;
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,135 @@
 import React from "react";
+import axios from "axios";
+import Switcher from "../components/Switcher";
+import { useAuth } from "../hooks/useAuth";
 
+axios.defaults.baseURL = "http://localhost:8000";
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const { login } = useAuth();
+  const logIn = async (email, password) => {
+    await axios
+      .post("/api/students/login", {
+        email,
+        passwrd: password,
+      })
+      .then(async (res) => {
+        console.log(res);
+        if (res.data.auth) {
+          await login({
+            email,
+            fullName: res.data.studentfullName,
+          });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("Incorrect email or password");
+      });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const data = new FormData(form);
+    if (data.get("email") === "" || data.get("password") === "") {
+      alert("Please fill all the fields");
+      return;
+    }
+
+    const email = data.get("email");
+    const password = data.get("password");
+
+    logIn(email, password);
+  };
+  return (
+    <div className='flex flex-col items-center justify-start px-6 py-8 mx-auto md:h-screen lg:py-0'>
+      <div className='self-end p-4'>
+        <Switcher />
+      </div>
+      <section className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-neutral-800 dark:border-neutral-700'>
+        <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
+          <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
+            Log in
+          </h1>
+          <form
+            onSubmit={handleSubmit}
+            method='POST'
+            className='space-y-4 md:space-y-6'
+          >
+            <div>
+              <label
+                htmlFor='email'
+                className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+              >
+                Your email
+              </label>
+              <input
+                type='email'
+                name='email'
+                id='email'
+                className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-neutral-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                placeholder='name@univ-tlemcen.dz'
+                required={true}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor='password'
+                className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+              >
+                Password
+              </label>
+              <input
+                type='password'
+                name='password'
+                id='password'
+                placeholder='••••••••'
+                className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-neutral-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                required={true}
+              />
+            </div>
+            <div className='flex items-center justify-between'>
+              <div className='flex items-start'>
+                <div className='flex items-center h-5'></div>
+              </div>
+              <a
+                href='#'
+                className='text-sm font-medium text-primary-600 hover:underline dark:text-primary-500'
+              >
+                Forgot password?
+              </a>
+            </div>
+            <button
+              type='submit'
+              className='w-full Light bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+            >
+              Log in
+            </button>
+            <p className='text-sm font-light text-gray-500 dark:text-gray-400'>
+              Don't have an account?{" "}
+              <a
+                href='/signup'
+                className='font-medium text-primary-600 hover:underline dark:text-primary-500'
+              >
+                Sign up
+              </a>
+            </p>
+            <p className='text-sm font-light text-gray-500 dark:text-gray-400'>
+              <a
+                href='#'
+                className='font-medium text-primary-600 hover:underline dark:text-primary-500'
+              >
+                Log in as Teacher
+              </a>
+            </p>
+          </form>
+        </div>
+      </section>
+      <p className='text-center my-4 text-neutral-900/75 dark:text-neutral-100/75 text-sm'>
+        © 2024 by UNIVLEARN. All rights reserved. <br />
+        Designed with ❤️ by UNIVLEARN Team
+      </p>
+    </div>
+  );
 }
 
 export default LoginPage;
