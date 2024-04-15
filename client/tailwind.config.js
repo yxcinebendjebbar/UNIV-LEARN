552afd3645d42d394e901652ddc27db8b99cfbdd
Yacine Bendjebbar <106481756+yxcinebendjebbar@users.noreply.github.com>
2024-04-15 12:15:26 +0100

    feat: added filter
    feat: added filter by search text, faculty, department, specialty

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,16 @@
+// tailwind.config.js
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    // ...
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   darkMode: "selector",
-  plugins: [],
+  plugins: [nextui()],
 };
